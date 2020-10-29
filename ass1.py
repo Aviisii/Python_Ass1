544f22ca2424bfc451dcb2ce34ae201920f10f0f
AVISI <kyawthetsoe1011@gmail.com>
2020-10-29 18:32:07 +0630

    Chapter3

--- ass1.py
+++ ass1.py
@@ -1,6 +1,6 @@
 #Chapter1
 #1
-'''
+
 a = input("Enter Your Name: ")
 print("Hello",a)
 
@@ -67,7 +67,7 @@ if j==0:
         print("It is a leap year")
 else:
     print("It is not a leap year")
-'''
+
 
 #5
 m = int(input("Enter your marks: "))
@@ -84,3 +84,50 @@ if m >= 60:
         print("Grade D")
 else:
     print("Grade F")
+
+#Chapter 3
+#1
+n = int(input("Enter a number: "))
+for i in range (1, 13):
+    print(n,"x",i,"=",n*i)
+
+#2
+p = int(input("Enter a number: "))
+q = 1
+if p < 0:
+    print("Factroial does not exit")
+elif p ==0:
+    print("The factorial of 0 is 1")
+else:
+    for r in range(1,p+1):
+        q = q * r
+        print ("The factorial of",p,"is",q)
+
+#3
+s = int(input("Enter digits: "))
+t = 0
+while s > 0:
+    v = s%10
+    t = t+v
+    s = int(s/10)
+    print("Sum of all digits: ", t)
+#crd google
+
+#4
+w = input("Enter any number: ")
+x = w[::-1]
+if x == w:
+    print("It is palindromic")
+else:
+    print("It is not a palindromic")
+
+#5
+y = int(input("Enter a number: "))
+z = int(input("Enter next number: "))
+def hcf(y, z):
+    while(z):
+        y, z = z, y%z
+    return y
+print("The HCF or GCD of your number: ",hcf(y,z))
+
+

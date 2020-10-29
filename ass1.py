e6b9c2f0810cebf790b2c53940c64f677a8020f8
AVISI <kyawthetsoe1011@gmail.com>
2020-10-29 12:58:15 +0630

    Chapter2

--- ass1.py
+++ ass1.py
@@ -21,9 +21,66 @@ minute = hr//60
 second = hr%60
 print(" Hour: ",hour ,"\n Minute: ",minute, "\n Second: ",second)
 
-'''
 #5
 f = int(input("Enter a Number: "))
 g = int(input("Enter next Number: "))
 f, g = g, f
 print("After Swapping\n" "First variable is: ",f ,"\nSecond variable is: ",g)
+
+
+
+#Chapter 2
+#1
+h = int(input("Enter a number: "))
+remain = h % 2
+if remain >0:
+    print("This is an odd number.")
+else:
+    print("This is an even number.")
+
+#2
+num1 = int(input("Enter an integer: "))
+num2 = int(input("Enter next integer: "))
+if num1 > num2:
+    print(num1, "is the largest.")
+else:
+    print(num2, "is the largest.")
+
+#3
+i_1 = int(input("Enter an integer: "))
+i_2 = int(input("Enter next integer: "))
+i_3 = int(input("Enter last integer: "))
+print("The largest number: ",max(i_1, i_2, i_3))
+
+#4
+i = int(input("Enter a year: "))
+j = i%4
+k = i%100
+l = i%400
+if j==0:
+    if k==0:
+        if l==0:
+            print("It is a leap year.")
+        else:
+            print("It is not a leap year.")
+    else:
+        print("It is a leap year")
+else:
+    print("It is not a leap year")
+'''
+
+#5
+m = int(input("Enter your marks: "))
+if m >= 60:
+    if m >= 70:
+        if m >= 80:
+            if m >= 90:
+                print("Grade A")
+            else:
+                print("Grade B")
+        else:
+            print("Grade C")
+    else:
+        print("Grade D")
+else:
+    print("Grade F")
